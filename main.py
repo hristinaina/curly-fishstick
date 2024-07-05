23819f43e7a64160d0b8476727532376c11557ea
Hristina Adamovic <hristinacina@gmail.com>
2024-07-05 10:36:11 +0200

    added oversampling

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.decomposition import PCA
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
-from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.svm import SVC
+from sklearn.ensemble import BaggingClassifier, VotingClassifier, AdaBoostClassifier, RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from imblearn.over_sampling import ADASYN
 
 
 def load_data(file='data/healthcare-dataset-stroke-data.csv'):
@@ -21,12 +24,17 @@ def handle_missing_values(df):
     return df
 
 
-#todo what to do with glucose
 def remove_outliers(df):
     df = df[(df['bmi'] >= 13) & (df['bmi'] <= 55)]
     df = df[(df['gender'] != 'Other')]
     df = df[(df['work_type'] != 'Never_worked')]
     return df
+
+
+def oversampling(X, y):
+    adasyn = ADASYN(random_state=42)
+    X_resampled, y_resampled = adasyn.fit_resample(X, y)
+    return X_resampled, y_resampled
 
 
 def encode(X):
@@ -37,21 +45,23 @@ def encode(X):
     return X
 
 
+def fit_model(X_train, y_train):
+    model = RandomForestClassifier(n_estimators=200, max_depth=10, random_state=42, class_weight='balanced_subsample')
+    model.fit(X_train, y_train)
+    return model
+
+
 def reduction(x_train, x_test):
     pca = PCA(n_components=min(x_train.shape[0], x_train.shape[1]))
     x_train_reduced = pca.fit_transform(x_train)
     x_test_reduced = pca.transform(x_test)
+
     return x_train_reduced, x_test_reduced
 
 
-def fit_model(x_train, y_train):
-    model = SVC(kernel='linear', random_state=42)
-    model.fit(x_train, y_train)
-    return model
-
-
-def evaluate(y_true, y_pred):
-    print(classification_report(y_true, y_pred))
+def evaluate(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    print(classification_report(y_test, y_pred))
 
 
 def main():
@@ -61,15 +71,14 @@ def main():
 
     X, y = separate_features(df)
     X = encode(X)
+    X, y = oversampling(X, y)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
 
-    #X_train, X_test = reduction(X_train, X_test)
+    X_train, X_test = reduction(X_train, X_test)
     model = fit_model(X_train, y_train)
 
-    y_pred = model.predict(X_test)
-
-    evaluate(y_test, y_pred)
+    evaluate(model, X_test, y_test)
 
 
 if __name__ == '__main__':
